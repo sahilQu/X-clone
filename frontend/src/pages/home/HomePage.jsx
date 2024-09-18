5808ff525d02fc3e25c0007e991fbc80ffc2d113
Md Sahi <sahileng2001@gmail.com>
2024-09-18 17:16:39 +0530

    add

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
 						}
 						onClick={() => setFeedType("forYou")}
 					>
-						For me
+						For me you
 						{feedType === "forYou" && (
 							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
 						)}
